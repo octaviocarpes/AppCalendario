e79d63a3555f49abd8a6b62134533ee069e5cf1a
octavio <octavio.carpes10@gmail.com>
2017-09-27 09:15:26 -0300

    segundo push, agora vai

---
+++ src/com/company/Calendario.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Calendario {
+    private String[] meses;
+    private String[] semana;
+}
---
+++ src/com/company/Evento.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Evento {
+    private String nomeDoEvento;
+    private String dataDoEvento;
+
+    public Evento(String nomeDoEvento, String dataDoEvento) {
+        this.nomeDoEvento = nomeDoEvento;
+        this.dataDoEvento = dataDoEvento;
+    }
+
+    public String getNomeDoEvento() {
+        return nomeDoEvento;
+    }
+
+    public void setNomeDoEvento(String nomeDoEvento) {
+        this.nomeDoEvento = nomeDoEvento;
+    }
+
+    public String getDataDoEvento() {
+        return dataDoEvento;
+    }
+
+    public void setDataDoEvento(String dataDoEvento) {
+        this.dataDoEvento = dataDoEvento;
+    }
+}
